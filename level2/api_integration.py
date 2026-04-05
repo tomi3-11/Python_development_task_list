01db18b06e5d6a4f444fc1f242c493c773494122
tom <didymusthomas6@gmail.com>
2026-04-06 00:36:20 +0300

    setup api-integration

--- level2/api_integration.py
+++ level2/api_integration.py
@@ -1,9 +1,18 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
 def main():
     pass
 
 
 def api_integration(city):
-    pass
+    url = ""
+    data = requests.get()
 
 
 if __name__ == "__main__":
